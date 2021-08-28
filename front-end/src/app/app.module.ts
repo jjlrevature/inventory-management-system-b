00bea7600cf2f5ee404dab1ff6ea652dbf15ac28
Angela <angela.lee@revature.net>
2021-08-28 00:07:03 -0400

    Created basic html file and angular components

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductComponent } from './add-product/add-product.component';
+import { ViewAllProductsComponent } from './view-all-products/view-all-products.component';
+import { AddStockDetailComponent } from './add-stock-detail/add-stock-detail.component';
+import { ViewStocksBelowThresholdComponent } from './view-stocks-below-threshold/view-stocks-below-threshold.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductComponent,
+    ViewAllProductsComponent,
+    AddStockDetailComponent,
+    ViewStocksBelowThresholdComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
---
+++ front-end/src/app/view-stocks-below-threshold/view-stocks-below-threshold.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-stocks-below-threshold',
+  templateUrl: './view-stocks-below-threshold.component.html',
+  styleUrls: ['./view-stocks-below-threshold.component.css']
+})
+export class ViewStocksBelowThresholdComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
