bad26239940da3ad74064b806d80045c6dccc78d
alec-patterson <alec.patterson@revature.net>
2021-09-02 15:51:29 -0700

    update

--- projectTwo/src/main/java/project/two/dao/ProductDAO.java
+++ projectTwo/src/main/java/project/two/dao/ProductDAO.java
@@ -3,6 +3,7 @@ package project.two.dao;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import project.two.models.Product;
 
