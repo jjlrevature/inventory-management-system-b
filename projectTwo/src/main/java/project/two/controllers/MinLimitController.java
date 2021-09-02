e8dc4c5156f1fc5b715dc68a239c0382c645b77f
Jesse Lauesen <jesse.lauesen@revature.net>
2021-09-02 13:33:01 -0500

    update

--- projectTwo/src/main/java/project/two/controllers/MinLimitController.java
+++ projectTwo/src/main/java/project/two/controllers/MinLimitController.java
@@ -7,6 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,7 +16,7 @@ import org.springframework.web.client.RestTemplate;
 import project.two.models.Product;
 import project.two.services.ProductService;
 
-
+@CrossOrigin(origins="http://localhost:4200")
 @RestController
 @RequestMapping(path="/under-min-limit", produces="application/json")
 public class MinLimitController {
