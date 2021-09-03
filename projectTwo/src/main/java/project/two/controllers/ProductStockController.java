334a1d12b1027f7a855032dd817057ba187d5fb5
Jesse Lauesen <jesse.lauesen@revature.net>
2021-09-03 13:51:23 -0500

    update

--- projectTwo/src/main/java/project/two/controllers/ProductStockController.java
+++ projectTwo/src/main/java/project/two/controllers/ProductStockController.java
@@ -31,30 +31,11 @@ public class ProductStockController {
 	@Autowired
 	private ProductDAO pDao;
 	
-	private int getTotal(List<ProductStock> plist) {
-		int total = 0;
-		for(int x = 0; x < plist.size(); x++) {
-			int newQuantity = plist.get(x).getQuantity();
-			if(newQuantity < 0) {				
-				total = total - Math.abs(newQuantity);
-			} else {
-				total = total + newQuantity;							
-			}
-		}		
-		return total;
-	}
+	
 
 	@GetMapping
 	public List<ProductStock> getStock(){
 		return psManager.getStock();
-	}
-	
-	@CrossOrigin(origins = "http://localhost:4200")
-	@GetMapping(path="/{productId}",produces="application/json")
-	public int getItemCurrentStockById(@PathVariable int productId) {
-		List<ProductStock> product = pDao.getById(productId).getStock();
-		int total = getTotal(product);
-		return total;		
 	}
 	
 	@PostMapping(value="/add",consumes="application/json")
@@ -72,95 +53,5 @@ public class ProductStockController {
 		}
 	}
 	
-//	@PostMapping(value="/add2",consumes="application/json")
-//	public ResponseEntity<ProductStock> createStock2(@RequestBody ProductStock stock) {
-//		try {
-//			Product proj = dao.findById(new Integer(2)).get();
-//			stock.setProduct(proj);
-//			Date time = new Date();
-//			stock.setDateOfTrans(time);
-//			ProductStock add = pDao.save(stock);
-//			return new ResponseEntity<>(add, HttpStatus.CREATED);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//	}
-//	
-//	@PostMapping(value="/add3",consumes="application/json")
-//	public ResponseEntity<ProductStock> createStock3(@RequestBody ProductStock stock) {
-//		try {
-//			Product proj = dao.findById(new Integer(3)).get();
-//
-//			stock.setProduct(proj);
-//			Date time = new Date();
-//			stock.setDateOfTrans(time);
-//			ProductStock add = repo		
-//					.save(stock);
-//			return new ResponseEntity<>(add, HttpStatus.CREATED);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//	}
-//	
-//	@PostMapping(value="/add3",consumes="application/json")
-//	public ResponseEntity<ProductStock> createStock4(@RequestBody ProductStock stock) {
-//		try {
-//			Product proj = dao.findById(new Integer(4)).get();
-//
-//			stock.setProduct(proj);
-//			Date time = new Date();
-//			stock.setDateOfTrans(time);
-//			ProductStock add = repo		
-//					.save(stock);
-//			return new ResponseEntity<>(add, HttpStatus.CREATED);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//	}
-//	
-//	@PostMapping(value="/add3",consumes="application/json")
-//	public ResponseEntity<ProductStock> createStock5(@RequestBody ProductStock stock) {
-//		try {
-//			Product proj = dao.findById(new Integer(5)).get();
-//
-//			stock.setProduct(proj);
-//			Date time = new Date();
-//			stock.setDateOfTrans(time);
-//			ProductStock add = repo		
-//					.save(stock);
-//			return new ResponseEntity<>(add, HttpStatus.CREATED);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
-//		}
-//	}
 	
-	@GetMapping(path="/ps5",produces="application/json")
-	public int[] getPs5Stock() {
-		List<ProductStock> ps5List = psManager.getPlaystation5InStock();
-		int total = getTotal(ps5List);
-		int[] returnObj = {1,total};
-		return returnObj;
-	}
-	
-	@GetMapping(path="/xbx",produces="application/json")
-	public int[] getXbxStock() {
-		List<ProductStock> xbxList = psManager.getXboxSeriesXInStock();
-		int total = getTotal(xbxList);
-		int[] returnObj = {2,total};
-		return returnObj;
-	}
-	
-	@GetMapping(path="/ns",produces="application/json")
-	public int[] getNsStock() {
-		List<ProductStock> nsList = psManager.getNintendoSwitchInStock();	
-		int total = getTotal(nsList);
-		int[] returnObj = {3,total};
-		return returnObj;
-	}
-	
-
 }
