97a52e6952cc4eb118075b66bef753fcb8c22107
alee-revature <angela.lee@revature.net>
2021-09-03 05:21:47 -0400

    Update ProductController.java

--- projectTwo/src/main/java/project/two/controllers/ProductController.java
+++ projectTwo/src/main/java/project/two/controllers/ProductController.java
@@ -1,5 +1,7 @@
 package project.two.controllers;
 
+import java.util.List;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
