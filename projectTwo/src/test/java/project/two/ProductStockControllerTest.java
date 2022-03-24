a8303af42920a22032daa79d3f9b5f045f190ebd
jjlrevature <jesse.lauesen@revature.net>
2022-03-24 10:24:15 -0500

    Update ProductStockControllerTest.java

--- projectTwo/src/test/java/project/two/ProductStockControllerTest.java
+++ projectTwo/src/test/java/project/two/ProductStockControllerTest.java
@@ -1,11 +1,18 @@
 package project.two;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import project.two.models.ProductStock;
 
 @TestMethodOrder(value = MethodOrderer.OrderAnnotation.class)
 public class ProductStockControllerTest extends AbstractTest {
@@ -22,28 +29,88 @@ public class ProductStockControllerTest extends AbstractTest {
 	@Order(1)
 	@DisplayName("**** test addProduct in ProductStockController")
 	void testAddProductStock() throws Exception {
+		//expected status
+		int expectedStatus = 201;
 		
+		// create mock productStock and convert to json
+		ProductStock mock = new ProductStock();
+		mock.setWarehouseStockId(productStock_id);
+		String jsonMock = super.mapToJson(mock);
+		
+		// run mock transaction
+		MvcResult mvc = mockMvc.perform(MockMvcRequestBuilders.post(uri+"/add/"+productStock_id)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(jsonMock)).andReturn();
+		
+		//receive result status and assert
+		int actualStatus = mvc.getResponse().getStatus();
+		assertEquals(expectedStatus, actualStatus);
 	}
 	
 	@Test
 	@Order(2)
 	@DisplayName("**** test addProduct in ProductStockController when ProductStock already exists")
 	void testAddProductStockError() throws Exception {
+		//expected status
+		int expectedStatus = 409;
 		
+		// create mock productStock and convert to json
+		ProductStock mock = new ProductStock();
+		mock.setWarehouseStockId(productStock_id);
+		String jsonMock = super.mapToJson(mock);
+		
+		// run mock transaction
+		MvcResult mvc = mockMvc.perform(MockMvcRequestBuilders.post(uri+"/add/"+productStock_id)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(jsonMock)).andReturn();
+		
+		//receive result status and assert
+		int actualStatus = mvc.getResponse().getStatus();
+		assertEquals(expectedStatus, actualStatus);
 	}
 	
 	@Test
 	@Order(3)
 	@DisplayName("**** test updateProduct in ProductStockController")
 	void testUpdateProductStock() throws Exception {
+		// expected status
+		int expectedStatus = 202;
 		
+		// mock product and map to json
+		ProductStock mock = new ProductStock();
+		mock.setWarehouseStockId(productStock_id);
+		String jsonMock = super.mapToJson(mock);
+		
+		// perform mock transaction
+		MvcResult mvc = mockMvc.perform(MockMvcRequestBuilders.put(uri+"/updateProductStock/"+productStock_id)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(jsonMock)).andReturn();
+		
+		// returned status and assert
+		int actualStatus = mvc.getResponse().getStatus();
+		assertEquals(expectedStatus, actualStatus);
 	}
 	
 	@Test
 	@Order(7)
 	@DisplayName("**** test updateProduct in ProductStockController when ProductStock does not exist")
 	void testUpdateProductStockError() throws Exception {
+		// expected status
+		int expectedStatus = 404;
 		
+		// mock product and map to json
+		ProductStock mock = new ProductStock();
+		mock.setWarehouseStockId(productStock_id);
+		String jsonMock = super.mapToJson(mock);
+		
+		// perform mock transaction
+		MvcResult mvc = mockMvc.perform(MockMvcRequestBuilders.put(uri+"/updateProductStock/"+productStock_id)
+				.contentType(MediaType.APPLICATION_JSON_VALUE)
+				.content(jsonMock)).andReturn();
+		
+		// returned status and assert
+		int actualStatus = mvc.getResponse().getStatus();
+		assertEquals(expectedStatus, actualStatus);
 	}
 	
 	@Test
